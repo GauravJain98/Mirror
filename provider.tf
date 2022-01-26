8d6c9675b9e773226d7394044c096f6fb38c3e2c
Gaurav Jain <gauravcdj.98@gmail.com>
2022-01-27 00:07:07 +0530

    Add terraform scripts

--- provider.tf
+++ provider.tf
@@ -1,3 +1,10 @@
 provider "aws" {
   region  = var.region
 }
+
+git init
+git add .        
+git commit -m "Add terraform scripts"
+git branch -M master
+git remote add origin git@github.com:GauravJain98/Mirror.git
+git push -u origin master
